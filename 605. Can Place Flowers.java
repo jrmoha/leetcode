e8cd8a4f664d833bcf2f97d04828e4fbf9c8cecc
Mostafa <mostafa.dash34@gmail.com>
2023-04-02 17:44:51 +0200

    Update 605. Can Place Flowers.java

--- 605. Can Place Flowers.java
+++ 605. Can Place Flowers.java
@@ -5,8 +5,7 @@ class Solution {
         for (int i = 0; i < size && cnt < n; i++) {
             if (flowerbed[i] == 0) {
                 int nxt=i+1<size?flowerbed[i+1]:flowerbed[i];
-                int prv=i-1>=0?flowerbed[i-1]:flowerbed[i
-                        ];
+                int prv=i-1>=0?flowerbed[i-1]:flowerbed[i];
                 if(nxt==0&&prv==0){
                     cnt++;
                     flowerbed[i]=1;
